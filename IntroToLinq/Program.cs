df59283e64aacbb17b72884c9064c0139e176a1d
Carlos Diones <carlosdiones28@gmail.com>
2023-01-02 21:14:56 -0300

    Add IntroToLinq

---
+++ IntroToLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿class IntroToLINQ
+{
+    static void Main()
+    {
+        int[] numbers = new int[7] {0, 1, 2, 3, 4, 5, 6};
+
+        var numQuery = 
+            from num in numbers
+            where (num % 2) == 0
+            select num;
+
+            foreach (int num in numQuery)
+            {
+                Console.Write("{0, 1}", num);
+            }
+    }
+
+}
